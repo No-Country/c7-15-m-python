db9cf28eca9eeb3b653dfe00c9139d051ba9cdad
hamerzin <dipaolo@gmail.com>
2022-10-03 21:49:11 -0300

    FEAT: Limpieza de codigo

--- Kecomer/Kecomer/settings.py
+++ Kecomer/Kecomer/settings.py
@@ -117,7 +117,7 @@ WSGI_APPLICATION = 'Kecomer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = db.SQLITE
+DATABASES = db.POSTGRESQL
 
 
 # Password validation
--- Kecomer/users/genericviews.py
+++ Kecomer/users/genericviews.py
@@ -1,11 +1,5 @@
-from users.serializers import UserCreationSerializer, UsersSerializers
-from rest_framework.generics import ListAPIView
+from users.serializers import UsersSerializers
 from users.models import User
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
 

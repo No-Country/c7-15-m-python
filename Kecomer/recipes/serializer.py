e320e36de0c11be283f4b3777c2a7990fbe18481
Marck <marvin.roses@gmail.com>
2022-10-19 18:33:39 -0500

    FIX: add ID serializer

--- Kecomer/recipes/serializer.py
+++ Kecomer/recipes/serializer.py
@@ -24,4 +24,4 @@ class RecipesSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = RecipesModel
-        fields = ["title", "link_video", "image", "category", "timeday", 'difficulty', 'duration', "like", "description", 'ingredients', 'quantity', 'instruction']
+        fields = ["id","title", "link_video", "image", "category", "timeday", 'difficulty', 'duration', "like", "description", 'ingredients', 'quantity', 'instruction']
